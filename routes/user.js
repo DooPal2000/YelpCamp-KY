8bd17a31ed94d9c764195146092bb3e209c3e570
Kyoung Yeob <kkyub1313@gmail.com>
2024-02-28 01:08:57 +0900

    Cp 51. 524강, 로그인 경로

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
+const passport = require('passport');
 const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
 
@@ -23,6 +24,16 @@ router.post('/register', catchAsync(async(req,res) => {
     }
     console.log(registerUser);
 }));
+
+router.get('/login',  (req,res)=> {
+    res.render('users/login');    
+});
+
+
+router.post('/login', passport.authenticate('local', { failureFlash: true, failureRedirect: '/login' }), (req,res)=> {
+    req.flash('success', 'welcome Back!');
+    res.redirect('/campgrounds');    
+})
 
 
 module.exports = router;
