4d83127428ecbd0d2c2165c5c22311ed6771f1bc
Kyoung Yeob <kkyub1313@gmail.com>
2024-03-12 00:01:47 +0900

    Cp 53. 540강, 경로 재설정을 위한 방법

--- routes/user.js
+++ routes/user.js
@@ -7,9 +7,18 @@ const User = require('../models/user');
 const users = require('../controllers/users');
 
 
-router.get('/register', users.renderRegister);
+router.route('/register')
+    .get(users.renderRegister)
+    .post(catchAsync(users.register));
 
-router.post('/register', catchAsync(users.register));
+
+// router.get('/register', users.renderRegister);
+// router.post('/register', catchAsync(users.register));
+
+router.route('/login')
+
+
+
 
 // PASSPORT 보안 강화로 인해 로그인 시 세션 초기화, 아래 코드 사용 
 // 이렇게 수정하면, 최신 버전의 Passport.js를 사용해도 애플리케이션에서 사용자가 로그인 페이지 전에 방문 중이던 페이지로 정확하게 리디렉션됩니다.
