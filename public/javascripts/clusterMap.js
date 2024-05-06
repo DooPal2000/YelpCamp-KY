5256ebafee6bf2fff58e813981c3ec7191695d93
Kyoung Yeob <kkyub1313@gmail.com>
2024-05-07 00:18:02 +0900

    Cp 57. 578~579강, 인라인 맵 스타일 제거하기

--- public/javascripts/clusterMap.js
+++ public/javascripts/clusterMap.js
@@ -1,14 +1,17 @@
 mapboxgl.accessToken = mapToken;
-
 const map = new mapboxgl.Map({
-    container: 'map',
-    // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
-    style: 'mapbox://styles/mapbox/dark-v11',
-    center: [-103.5917, 40.6699],
+    container: 'cluster-map',
+    style: 'mapbox://styles/mapbox/light-v10',
+    center: [-103.59179687498357, 40.66995747013945],
     zoom: 3
 });
 
-map.on('load', () => {
+map.addControl(new mapboxgl.NavigationControl());
+
+
+
+
+map.on('load', function () {
     // Add a new source from our GeoJSON data and
     // set the 'cluster' option to true. GL-JS will
     // add the point_count property to your source data.
@@ -28,7 +31,7 @@ map.on('load', () => {
         source: 'campgrounds',
         filter: ['has', 'point_count'],
         paint: {
-            // Use step expressions (https://docs.mapbox.com/style-spec/reference/expressions/#step)
+            // Use step expressions (https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-step)
             // with three steps to implement three types of circles:
             //   * Blue, 20px circles when point count is less than 100
             //   * Yellow, 30px circles when point count is between 100 and 750
@@ -36,7 +39,7 @@ map.on('load', () => {
             'circle-color': [
                 'step',
                 ['get', 'point_count'],
-                '#03A9F4',
+                '#00BCD4',
                 10,
                 '#2196F3',
                 30,
@@ -60,7 +63,7 @@ map.on('load', () => {
         source: 'campgrounds',
         filter: ['has', 'point_count'],
         layout: {
-            'text-field': ['get', 'point_count_abbreviated'],
+            'text-field': '{point_count_abbreviated}',
             'text-font': ['DIN Offc Pro Medium', 'Arial Unicode MS Bold'],
             'text-size': 12
         }
@@ -73,21 +76,21 @@ map.on('load', () => {
         filter: ['!', ['has', 'point_count']],
         paint: {
             'circle-color': '#11b4da',
-            'circle-radius': 5,
+            'circle-radius': 4,
             'circle-stroke-width': 1,
             'circle-stroke-color': '#fff'
         }
     });
 
     // inspect a cluster on click
-    map.on('click', 'clusters', (e) => {
+    map.on('click', 'clusters', function (e) {
         const features = map.queryRenderedFeatures(e.point, {
             layers: ['clusters']
         });
         const clusterId = features[0].properties.cluster_id;
         map.getSource('campgrounds').getClusterExpansionZoom(
             clusterId,
-            (err, zoom) => {
+            function (err, zoom) {
                 if (err) return;
 
                 map.easeTo({
@@ -102,8 +105,7 @@ map.on('load', () => {
     // the unclustered-point layer, open a popup at
     // the location of the feature, with
     // description HTML from its properties.
-    map.on('click', 'unclustered-point', (e) => {
-        console.log(e.features[0].properties.popUpMarkup);
+    map.on('click', 'unclustered-point', function (e) {
         const { popUpMarkup } = e.features[0].properties;
         const coordinates = e.features[0].geometry.coordinates.slice();
 
@@ -120,10 +122,10 @@ map.on('load', () => {
             .addTo(map);
     });
 
-    map.on('mouseenter', 'clusters', () => {
+    map.on('mouseenter', 'clusters', function () {
         map.getCanvas().style.cursor = 'pointer';
     });
-    map.on('mouseleave', 'clusters', () => {
+    map.on('mouseleave', 'clusters', function () {
         map.getCanvas().style.cursor = '';
     });
 });
