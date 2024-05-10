6784b0beff26b6ad23776accc0b32d5c437223c8
Kyoung Yeob <kkyub1313@gmail.com>
2024-05-10 14:19:03 +0900

    오타수정

--- middleware.js
+++ middleware.js
@@ -35,7 +35,7 @@ module.exports.isAuthor = async (req, res, next) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
     if (!campground.author.equals(req.user._id)) {
-        req.flash('error', 'You do not have permission to to that!');
+        req.flash('error', 'You do not have permission to do that!');
         return res.redirect(`/campgrounds/${id}`);
     }
     next();
